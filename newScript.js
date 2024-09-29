8383b68053fd1e9c6b71ec54762ce6993f8b3878
yamamot000 <167596865+yamamot000@users.noreply.github.com>
2024-09-29 18:43:32 +0800

    newScript.js

---
+++ newScript.js
@@ -0,0 +1,16 @@
+let queueNumber = 1;
+function generateQRCode() {
+    const qrCodeContainer = document.getElementById('qrcode');
+    qrCodeContainer.innerHTML = '';
+    const randomInt = Math.floor(Math.random() * 100000000) + 1; 
+    const qrCodeText = `https://example.com/${queueNumber}-${randomInt}`;
+    const qrCode = new QRCode(qrCodeContainer, {
+        text: qrCodeText,
+        width: 128,
+        height: 128
+    });
+    document.getElementById('queueNumber').innerText = `Queue: ${queueNumber}`;
+    queueNumber++;
+}
+setInterval(generateQRCode, 5000);
+generateQRCode();
